8d8e572fe7be4aed11c35d97d9c22fbb40a95a0e
Real Me <real@me>
2024-09-14 15:56:18 +0300

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,26 @@ LOCKFILE=config/.lockfile
 if [ ! -f $LOCKFILE ]
 then
   #generate Public & Private keys
-  cp /opt/xray/default-config/config.json opt/xray/config/config.json
+  cp /opt/xray/default-config/config.json /opt/xray/config/config.json
   echo "Generate public & private keys..."
   /opt/xray/xray x25519 > config/keys
 
   #Create files with Public & Private keys
-  awk '/Public/{print $3}' opt/xray/config/keys > opt/xray/config/public
-  awk '/Private/{print $3}' opt/xray/config/keys > opt/xray/config/private
+  awk '/Public/{print $3}' /opt/xray/config/keys > /opt/xray/config/public
+  awk '/Private/{print $3}' /opt/xray/config/keys > /opt/xray/config/private
 
   PRIVATE=$(cat config/private)
 
   #set private key in config.json
-  sed -i 's/"privateKey":.*/"privateKey": "'${PRIVATE}'",/' opt/xray/config/config.json
+  sed -i 's/"privateKey":.*/"privateKey": "'${PRIVATE}'",/' /opt/xray/config/config.json
 
   source scripts/add-client.sh --nickname admin
   #create lockfile
   touch $LOCKFILE
 fi
 
-sed -i 's/"dest":.*/"dest": "'${SNI}':443",/' opt/xray/config/config.json
-sed -i '/serverNames/{n;s/.*/\t\t\t\t"'${SNI}'"/}' opt/xray/config/config.json
+sed -i 's/"dest":.*/"dest": "'${SNI}':443",/' /opt/xray/config/config.json
+sed -i '/serverNames/{n;s/.*/\t\t\t\t"'${SNI}'"/}' /opt/xray/config/config.json
 
 #run proxy
 echo "XTLS reality starting..."
