79c7ca278f862375d5c1d4beed71479a933068f9
Jexperto <36307037+Jexperto@users.noreply.github.com>
2026-08-21 22:22:50 +0300

    Update get-client-url.sh

--- scripts/get-client-url.sh
+++ scripts/get-client-url.sh
@@ -9,4 +9,4 @@ UUID=$(cat config/clients/${1}/uuid)
 NICKNAME=$(cat config/clients/${1}/nickname)
 PUB_KEY=$(cat config/public)
 
-echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${1}#${NICKNAME}"
+echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=firefox&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${1}#${NICKNAME}"
